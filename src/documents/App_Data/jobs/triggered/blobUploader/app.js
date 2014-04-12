14c308dce1a4484ff5f236fe3507059391949db8
Omer Wazir <omer.wazir@gmail.com>
2014-04-12 09:00:22 -0700

    Blob Uploader works

--- src/documents/App_Data/jobs/triggered/blobUploader/app.js
+++ src/documents/App_Data/jobs/triggered/blobUploader/app.js
@@ -1,1 +1,13 @@
+var fs = require('fs');
 var azure = require('azure');
+var blobService = azure.createBlobService();
+var util = require('util');
+var cssFile = '../../../../styles/style.css'
+
+blobService.putBlockBlobFromFile('css', 'omerwazir.css', cssFile, {contentType:'text/css'}, function(error) {
+  if(!error) {
+    console.log('omerwazir.css uploaded');
+  } else {
+    console.error('Error: ' + error);
+  }
+});
