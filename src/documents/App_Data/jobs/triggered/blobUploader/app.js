51e0df2fe3d549b73d93b0d9ba682f43f7d2ec27
Omer Wazir <omer.wazir@gmail.com>
2014-04-13 14:30:35 -0700

    Changed how hash stream managed callback

--- src/documents/App_Data/jobs/triggered/blobUploader/app.js
+++ src/documents/App_Data/jobs/triggered/blobUploader/app.js
@@ -1,34 +1,35 @@
 var azure = require('azure');
-var async = require('async');
 var blobService = azure.createBlobService();
+var async = require('async');
+var through = require('through');
 var crypto = require('crypto');
 var fs = require('fs');
-var cssFile = '../../../../styles/style.css';
-var hash = crypto.createHash('md5');
 
-hash.setEncoding('base64');
+var cssFile = '../../../../styles/style.css';
 
 async.waterfall([
     function(callback){
         blobService.getBlobProperties('css', 'omerwazir.css',function(error,blob){
             if(error) callback(error);
-            if(blob) callback(null,blob);
+            if(blob) callback(null, blob.contentMD5);
         })
     },
-    function(blob, callback){
-    
-        var readStream = fs.createReadStream(cssFile);
-        
-        readStream.on('data',function(chunk){
-            hash.update(chunk)
-        })
-
-        readStream.on('end', function() {
+    function(blobContentMD5, callback){
+        var hash = crypto.createHash('md5');
+        hash.setEncoding('base64');
+        fs.createReadStream(cssFile).pipe(through(function write(chunk){
+            hash.update(chunk);
+        }, function end(){
             hash.end();
-            var md5sum = hash.read();
-            if(blob.contentMD5 !== md5sum ){
+            callback(null,blobContentMD5, hash.read());
+        }));
+        },
+    function(blobContentMD5, localContentMD5, callback){
+        console.log('BlobMD5: ' + blobContentMD5);
+        console.log('LocalFileMD5: ' + localContentMD5);
+        if(blobContentMD5 !== localContentMD5 ){
                     // Files don't match so PUT file to container
-                     blobService.putBlockBlobFromFile('css', 'omerwazir.css', cssFile,
+                blobService.putBlockBlobFromFile('css', 'omerwazir.css', cssFile,
                       {contentType: 'text/css'}, function(error, blockBlob, response) {
                           if(!error) {
                             callback(null, blockBlob.blob + ' PUT to container ' + 
@@ -40,8 +41,7 @@ async.waterfall([
             } else {
                 callback(null, 'Files match, nothing was uploaded');
             }
-        });
-    }
+        }
 ], function (err, result) {
     if(err) console.error('Error: '+ err );
    console.log('Result:' + result); 
