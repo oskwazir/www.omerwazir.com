4eefcc71507e860b70c9cf964cd3e74bc0f1b9ec
Omer Wazir <omer@thewazir.com>
2016-06-23 10:31:48 -0700

    Use standard http for now

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :static_blog, StaticBlog.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.omerwazir.com", port: 443],
+  url: [host: "www.omerwazir.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
