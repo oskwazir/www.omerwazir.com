01cc8565f457026c7948ab331c5170c80a0f44da
Omer Wazir <omer.wazir@gmail.com>
2017-04-06 00:04:01 -0700

    Update some dependencies

--- mix.exs
+++ mix.exs
@@ -29,10 +29,10 @@ defmodule StaticBlog.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
-     {:phoenix_html, "~> 2.4"},
+    [{:phoenix, "~> 1.2.1"},
+     {:phoenix_html, "~> 2.9.2"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11.0"},
+     {:gettext, "~> 0.13.1"},
      {:yamerl, "~> 0.4.0"},
      {:timex, "~> 3.1"},
      {:cowboy, "~> 1.0"},
