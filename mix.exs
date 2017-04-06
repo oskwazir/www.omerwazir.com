380800b4e744ba89ee7f8fb819b8c810e409f875
Omer Wazir <omer.wazir@gmail.com>
2017-04-06 00:08:27 -0700

    Fix typo

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule StaticBlog.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_html, "~> 2.9.2"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.13.1"},s
+     {:gettext, "~> 0.13.1"},
      {:yamerl, "~> 0.4.0"},
      {:timex, "~> 3.1"},
      {:cowboy, "~> 1.0"},
